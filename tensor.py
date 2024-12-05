728493669fe26fd8a6e820b6f0f309e325b43e10
Syed Noor Mohammed <nooralifirst@gmail.com>
2024-12-05 11:03:29 +0530

    Add Dot functionality and implement Backprop

--- tensor.py
+++ tensor.py
@@ -4,7 +4,9 @@ import numpy as np
 
 class Context:
     
-    def __init__(self):
+    def __init__(self, arg, *tensors):
+        self.arg = arg
+        self.parents = tensors
         self.saved_tensors = []
 
     def saved_for_bacward(self, *x):
@@ -12,13 +14,49 @@ class Context:
 
 class Tensor:
 
-    def __init__(self, data, _children=()):
+    def __init__(self, data):
+
+        if (type(data) is not np.ndarray):
+            raise ValueError("Data must be of type numpy.ndarray", data)
+            assert False
         self.data = data
-        self.grad = np.zeros_like(self.data)
+        self.grad = None
 
         # dont understand the use of this part
-        self._prev = set(_children)
+        self._ctx = None
+    def __str__(self) -> str:
+        return "Tensor %r with grad %r" % (self.data, self.grad)
+    
+    def backward(self, allow_fill=True):
 
+        print("Backward on", self)
+
+        if self._ctx is None:
+            return
+
+        if self.grad is None and allow_fill:
+            # fill the first gradient with ones
+            assert self.data.size == 1
+            self.grad = np.ones_like(self.data)
+        
+        assert self.grad is not None, "grad is None"
+
+        grads = self._ctx.arg.backward(self._ctx, self.grad)
+
+        if len(self._ctx.parents) == 1:
+            grads = [grads]
+        
+        for t, grad in zip(self._ctx.parents, grads):
+            if grad.shape != t.data.shape:
+                print("grad shape must match tensor shape in %r, %r != %r" % (self._ctx.arg, grad.shape, t.data.shape))
+                assert(False)
+            t.grad = grad
+            t.backward(allow_fill=False)
+    
+    def mean(self):
+        div = Tensor(np.array([1 / self.data.size]))
+        return self.sum().mul(div)
+        
 class Function:
     def apply(self, arg, *x):
         ctx = Context()
@@ -51,3 +89,14 @@ class Mulitply(Function):
     def backward(ctx, grad_output):
         a, b = ctx.saved_tensors
         return grad_output * b, grad_output * a
+
+class Dot(Function):
+    @staticmethod
+    def forward(ctx, input, weight):
+        ctx.saved_for_bacward(input, weight)
+        return input.dot(weight)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, weight = ctx.saved_tensors
+        return grad_output.dot(weight.T), input.Y.dot(grad_output)
