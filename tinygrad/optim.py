05ec0563fcdf4f4a97dd02d9389647d26570649f
Noor Mohammed Syed <87633406+RhFz1@users.noreply.github.com>
2024-12-10 23:41:06 +0530

    Update optim.py, add comments for understanding

--- tinygrad/optim.py
+++ tinygrad/optim.py
@@ -16,14 +16,17 @@ class Adam:
         self.lr = lr
         self.beta1 = beta1
         self.beta2 = beta2
-        self.eps = eps
+        self.eps = eps # Correction factor to address / 0
         self.t = 0
         self.m = [Tensor(np.zeros_like(p.data)) for p in params]
         self.v = [Tensor(np.zeros_like(p.data)) for p in params]
         
     def step(self):
+        # This tracks the steps of optimization, which are equivalent to the number of times you step().
         self.t += 1
+        # Only reason for looping is to address multiple params, which can be of diff. shapes.
         for i, p in enumerate(self.params):
+            # Calculating first and second momentum.
             self.m[i].data = self.beta1 * self.m[i].data + (1 - self.beta1) * p.grad
             self.v[i].data = self.beta2 * self.v[i].data + (1 - self.beta2) * p.grad * p.grad
             mhat = self.m[i].data / (1 - self.beta1**self.t)
